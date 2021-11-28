c7a1bc7260ea30cfc8256dd18dd9d6b0be0734b5
koalastyle <loveg50@163.com>
2021-11-28 22:11:10 +0800

    统一调用方法雏形

--- src/main/java/testDemo/demo1.java
+++ src/main/java/testDemo/demo1.java
@@ -3,33 +3,35 @@ package testDemo;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.example.colesspringbuilding.constant.DConstant;
 import com.example.colesspringbuilding.pojo.In.InHeadPojo;
 import com.example.colesspringbuilding.pojo.In.params.InParamsGeoCoding;
 import com.example.colesspringbuilding.pojo.OutGeocodesPojo;
 
-import java.util.HashMap;
-import java.util.Map;
-
 
 
 public class demo1 {
-    private static final String URL = "https://restapi.amap.com/v3/geocode/geo";
+
     public static void main(String[] args) {
         InParamsGeoCoding params = new InParamsGeoCoding();
         InHeadPojo in = new InHeadPojo();
         in.setUrl("https://restapi.amap.com/v3/geocode/geo");
         params.setKey(DConstant.GEO_KEY);
-        params.setAddress("北京市建国门大街3号");
+        params.setAddress("四川省成都市博物馆");
 
-        Map<String,Object> inParams = new HashMap<>();
-        inParams.put("address","成都市电子科技大学");
-        inParams.put("key", DConstant.GEO_KEY);
-        String response = HttpUtil.get(in.getUrl(),inParams);//TODO 工具类无法对obj格式的进行get
+
+//        Map<String,Object> ss = getJson(params);
+        String response = HttpUtil.get(in.getUrl(), getJson(params));
         OutGeocodesPojo out = new OutGeocodesPojo();
         out = JSON.parseObject(response,out.getClass());
         JSONObject json = JSON.parseObject(response);
-        System.out.println(json.toString(SerializerFeature.PrettyFormat));
+        System.out.println(out.toString());
+    }
+
+
+   static JSONObject getJson(InParamsGeoCoding o){
+        Object obj =  JSON.toJSON(o);
+        JSONObject json = JSON.parseObject(obj.toString());
+        return json;
     }
 }
