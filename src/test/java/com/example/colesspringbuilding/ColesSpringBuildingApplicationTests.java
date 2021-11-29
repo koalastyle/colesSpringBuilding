3c17b8ea26f92d85345c0d054fabccbcef237a4d
feiniao <664836141@qq.com>
2021-11-29 19:51:39 +0800

    修改测试类

--- src/test/java/com/example/colesspringbuilding/ColesSpringBuildingApplicationTests.java
+++ src/test/java/com/example/colesspringbuilding/ColesSpringBuildingApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.colesspringbuilding;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.example.colesspringbuilding.pojo.OutGeocodesPojo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ColesSpringBuildingApplicationTests {
+    @Value("${app.secret-key:}")
+    private String secretKey;
 
     @Test
     void contextLoads() {
+        OutGeocodesPojo outGeocodesPojo = new OutGeocodesPojo();
+        outGeocodesPojo.setInfo("success");
+        Object o = JSON.toJSON(outGeocodesPojo);
+        JSONObject jsonObject = JSON.parseObject(o.toString());
+        System.out.println(jsonObject);
     }
 
 }
