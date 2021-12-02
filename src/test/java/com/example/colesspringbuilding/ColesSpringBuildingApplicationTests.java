4b930c5d66f97c8bd446700c4fc5471b1a2e2f7b
feiniao <664836141@qq.com>
2021-12-02 23:21:42 +0800

    增加输出流测试方法

--- src/test/java/com/example/colesspringbuilding/ColesSpringBuildingApplicationTests.java
+++ src/test/java/com/example/colesspringbuilding/ColesSpringBuildingApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.colesspringbuilding;
 
+import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -13,6 +14,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.annotation.Resource;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.HashMap;
 
 @SpringBootTest
@@ -37,40 +42,52 @@ class ColesSpringBuildingApplicationTests {
 
 
         params.setKey(secretKey);
-        System.out.println("secretKey="+secretKey);
+        System.out.println("secretKey=" + secretKey);
         params.setAddress("四川省成都市博物馆|资阳市雁江区摩根时代");
 
-        if(params.getAddress().contains("|")){
+        if (params.getAddress().contains("|")) {
             params.setBatch("true");
         }
 
 
-        OutParamsGeoCoding outParam  = callUrlService.callUrl(params);
+        OutParamsGeoCoding outParam = callUrlService.callUrl(params);
         System.out.println(outParam.toString());
     }
 
     @Test
-    void staticMapTest(){
-        HashMap<String,Object> map = new HashMap<>();
-        map.put("key",secretKey);
-        map.put("zoom","5");
+    void staticMapTest() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("key", secretKey);
+        map.put("zoom", "5");
 //        map.put("location","104.644790,30.117735");
 //        map.put("size","large");
 //        map.put("markers","mid,0xFF0000,A:116.37359,39.92437;116.47359,39.92437");
-        map.put("markers","-1");
+        map.put("markers", "-1");
         String response = HttpUtil.get(DConstant.URL_STATIC_MAP, map);
         JSONObject json = JSON.parseObject(response);
         System.out.println(json.toString());
     }
 
 
-     JSONObject getJson(InParamsGeoCoding o){
-        Object obj =  JSON.toJSON(o);
+    JSONObject getJson(InParamsGeoCoding o) {
+        Object obj = JSON.toJSON(o);
         JSONObject json = JSON.parseObject(obj.toString());
         return json;
     }
 
-
+    @Test
+    void a() throws IOException {
+        String url = "https://restapi.amap.com/v3/staticmap?zoom=15&size=500*500&paths=10,0x0000ff,1,,:116.31604,39.96491;116.320816,39.966606;116.321785,39.966827;116.32361,39.966957&key=" + secretKey;
+        HttpResponse execute = HttpUtil.createGet(url).execute();
+        byte[] bytes = execute.bodyBytes();
+        FileOutputStream fos = new FileOutputStream("data.png");
+        FileChannel channel = fos.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(bytes.length);
+        buffer.put(bytes);
+        buffer.flip();
+        channel.write(buffer);
+        channel.close();
+    }
 
 
 }
