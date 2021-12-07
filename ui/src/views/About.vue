030eb708da0c0374d4c34a9ec564043bc4baaccc
FEINIAO233 <38801933+feiniao233@users.noreply.github.com>
2021-12-07 23:00:52 +0800

    前端增加vue router 及 axios依赖，添加axios get实例

---
+++ ui/src/views/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="test">
+      Context: {{ context }}
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "test",
+  data() {
+    return {context: "1"}
+  },
+  mounted() {
+    setInterval(() => {
+      get()
+    }, 1000)
+  }
+}
+
+function get() {
+  axios.get("https://httpbin.org/ip")
+      .then(function (response) {
+            console.log(response)
+          }
+      )
+      .catch(function (error) {
+        console.log(error)
+      })
+}
+</script>
